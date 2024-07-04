543c6a11c6a9e9d554f0753cd8088955bedf8cb0
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-04 00:05:40 -0400

    Implementación de ordenamiento de puntos por abscisa creciente

--- Calculo Numerico/Montana Rusa/csv_es.py
+++ Calculo Numerico/Montana Rusa/csv_es.py
@@ -16,8 +16,8 @@ def parametros_desde_csv(nombre_archivo):
     try:
         with open(nombre_archivo, newline="") as archivo:
             lector = iter(csv.reader(archivo))
-            valores_x = np.array([float(x) for x in next(lector)])
-            valores_y = np.array([float(x) for x in next(lector)])
+            valores_x = [np.float64(x) for x in next(lector)]
+            valores_y = [np.float64(x) for x in next(lector)]
             intervalo = next(lector)
     except OSError as e:
         raise type(e)("No se pudo leer el archivo") from e
@@ -30,24 +30,46 @@ def parametros_desde_csv(nombre_archivo):
         raise ValueError("El intervalo debe ser de tres valores")
     intervalo = (float(intervalo[0]), float(intervalo[1]), int(intervalo[2]))
 
+    valores_x, valores_y = ordenar_por_iesimo((valores_x, valores_y), 0)
+    anterior = np.float64("-inf")
+    for valor in valores_x:
+        if anterior == valor:
+            raise ValueError("No puede haber puntos con la misma coordenada 'x'")
+        anterior = valor
+
     return (valores_x, valores_y, np.linspace(*intervalo))
 
 def ordenar_por_iesimo(secuencias, indice):
-    if indice < 0 and indice >= len(secuencias):
+    """Ordena las secuencias tomando como referencia aquella en posición indice.
+
+    Se ordena crecientemente la secuencia en la posición dada por indice;
+    las demás secuencias se ordenan de forma tal que el i-ésimo elemento de
+    cada secuencia se reubique en la misma posición que el i-ésimo elemento
+    de la secuencia de referencia.
+    """
+
+    try:
+        secuencias[indice]
+    except IndexError:
         raise IndexError("Índice fuera de rango (0:%d): %d"
                          % (len(secuencias), indice))
 
+    return tuple(np.array(secuencia) for secuencia in zip(
+        * sorted(zip(*secuencias), key=(lambda tupla: tupla[indice]))  ))
+
+    # Otra forma más complicada
+    """
     referencia = secuencias[indice]
     largo = len(referencia)
-    for i in range(largo - 1):
-        valores = [secuencia[largo - 1] for secuencia in secuencias]
+    for i in range(1, largo):
+        valores = tuple(secuencia[i] for secuencia in secuencias)
         comparado = valores[indice]
-        for j in range(largo - 1, i, -1):
+        for j in range(i - 1, 0, -1):
+            if referencia[j] < comparado:
+                j += 1
+                break
             for secuencia in secuencias:
-                secuencia[j] = secuencia[j - 1]
-            if comparado < referencia[j]:
-                for k, secuencia in enumerate(secuencias):
-                    secuencia[j - 1] = valores[k]
-
-    pass  # Falta esto
-            
+                secuencia[j + 1] = secuencia[j]
+        for k, secuencia in enumerate(secuencias):
+            secuencia[j] = valores[k]
+    """
