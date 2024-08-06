f6417f96e20bb29627a838b747485f85d24da93e
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-08-06 00:48:50 -0400

    Ajustes varios en api de analisis de datos

--- Calculo Numerico/api/api_analisis_datos.py
+++ Calculo Numerico/api/api_analisis_datos.py
@@ -2,10 +2,16 @@ from analisis import *
 import os
 from base64 import b64encode
 
+from pydantic import BaseModel
 from fastapi import APIRouter, Body, Response, HTTPException
 from fastapi.responses import HTMLResponse
 
-router = APIRouter(prefix="/analisis")
+class PNGResponse(Response):
+    media_type = "image/png"
+
+class TablaImagen(BaseModel):
+    tabla: str
+    imagen: str
 
 DIR_TEMP = "temp"
 RUTA_DATOS = DIR_TEMP + "/datos_viento.txt"
@@ -15,6 +21,9 @@ except FileExistsError:
     pass
 EXCEPCION_DATOS_MAL_FORMADOS = HTTPException(422, detail=
                         [{"msg": "Datos mal formados. Cárguelos nuevamente"}])
+RESPUESTAS_ERRORES = {422: {"content": {"application/json": {}} }}
+
+router = APIRouter(prefix="/analisis", responses=RESPUESTAS_ERRORES)
 
 @router.put("/cargar-datos", status_code=204)
 def cargar_datos(datos: str = Body(media_type="text/plain")) -> None:
@@ -23,35 +32,35 @@ def cargar_datos(datos: str = Body(media_type="text/plain")) -> None:
     archivo.flush()
     archivo.close()
 
-@router.get("/dispersion-primeros-1000")
+@router.get("/dispersion-primeros-1000", response_class=PNGResponse)
 def dispersion_primeros_1000() -> Response:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
         raise EXCEPCION_DATOS_MAL_FORMADOS
     imagen = graficar_dispersion_primeros_1000(datos)
-    return Response(imagen, media_type="imagen/png")
+    return PNGResponse(imagen)
 
-@router.get("/dispersion-1000-mayor-rapidez")
+@router.get("/dispersion-1000-mayor-rapidez", response_class=PNGResponse)
 def dispersion_1000_mayor_rapidez() -> Response:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
         raise EXCEPCION_DATOS_MAL_FORMADOS
     imagen = graficar_dispersion_1000_mayor_rapidez(datos)
-    return Response(imagen, media_type="imagen/png")
+    return PNGResponse(imagen)
 
-@router.get("/histograma-rapidez")
+@router.get("/histograma-rapidez", response_class=PNGResponse)
 def histograma_rapidez() -> Response:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
         raise EXCEPCION_DATOS_MAL_FORMADOS
     imagen = graficar_histograma_rapidez(datos)
-    return Response(imagen, media_type="imagen/png")
+    return PNGResponse(imagen)
 
 @router.get("/rapidez_media_mensual")
-def rapidez_media_mensual():
+def rapidez_media_mensual() -> TablaImagen:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
@@ -60,7 +69,7 @@ def rapidez_media_mensual():
     return {"tabla": tabla, "imagen": b64encode(imagen)}
 
 @router.get("/tabla-rapidez-media-mensual", response_class=HTMLResponse)
-def tabla_rapidez_media_mensual():
+def tabla_rapidez_media_mensual() -> Response:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
@@ -68,12 +77,12 @@ def tabla_rapidez_media_mensual():
     tabla = tabla_desde_historico(rapidez_media_mensual_aux(datos))
     return tabla
 
-@router.get("/rapidez_media_mensual_anual")
-def rapidez_media_mensual_anual():
+@router.get("/rapidez_media_mensual_anual", response_class=PNGResponse)
+def rapidez_media_mensual_anual() -> Response:
     try:
         datos = leer_modelo(RUTA_DATOS)
     except (ValueError, FileNotFoundError):
         raise EXCEPCION_DATOS_MAL_FORMADOS
     imagen = graficar_historico_mensual(
         tabla_desde_historico_aux(rapidez_media_mensual_aux(datos)) )
-    return Response(imagen, media_type="imagen/png")
+    return PNGResponse(imagen)
